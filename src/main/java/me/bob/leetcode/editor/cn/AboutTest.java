205f1a48c995f1fd2b9a36e2a8393a80f150fa9f
sakiila <imbaobo@gmail.com>
2021-01-21 12:05:29 +0800

    387 字符串中的第一个唯一字符

--- src/main/java/me/bob/leetcode/editor/cn/AboutTest.java
+++ src/main/java/me/bob/leetcode/editor/cn/AboutTest.java
@@ -1,49 +1,10 @@
 package me.bob.leetcode.editor.cn;
 
-import java.util.Arrays;
-
 public class AboutTest {
     public static void main(String[] args) {
-        int[] tmp = new int[]{1, 3, 5, 2};
-        int[] tmp2 = new int[tmp.length + 2];
-
-        System.arraycopy(tmp, 0, tmp2, 1, tmp.length);
-        System.out.println(Arrays.toString(tmp2));
-
-        int[] ints = Arrays.copyOf(tmp, 5);
-        System.out.println("Arrays.toString(ints) = " + Arrays.toString(ints));
-
+        String a = "acb";
+        String b = "acb";
+        System.out.println(a == b);
     }
 
-    class UnionFind {
-        private int count = 0;
-        private int[] parent;
-
-        public UnionFind(int n) {
-            count = n;
-            parent = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-            }
-        }
-
-        public int find(int p) {
-            while (p != parent[p]) {
-                parent[p] = parent[parent[p]];
-                p = parent[p];
-            }
-            return p;
-        }
-
-        public void union(int p, int q) {
-            int rootP = find(p);
-            int rootQ = find(q);
-            if (rootP == rootQ) {
-                return;
-            }
-
-            parent[rootP] = rootQ;
-            count--;
-        }
-    }
 }
---
+++ src/main/java/me/bob/leetcode/editor/cn/FirstUniqueCharacterInAString.java
@@ -0,0 +1,53 @@
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
+//
+// 
+//
+// 示例： 
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+// 
+//
+// 
+//
+// 提示：你可以假定该字符串只包含小写字母。 
+// Related Topics 哈希表 字符串 
+// 👍 345 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 387 字符串中的第一个唯一字符
+ * 2021-01-21 11:17:16
+ * 思路： hashmap
+ */
+public class FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+        Solution solution = new FirstUniqueCharacterInAString().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int firstUniqChar(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (map.get(s.charAt(i)) == 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
