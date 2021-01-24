d0c6810d2eb3255db0ee9cc26336bcdd0011df9f
sakiila <imbaobo@gmail.com>
2021-01-25 07:45:06 +0800

    724 寻找数组的中心索引

---
+++ src/main/java/me/bob/leetcode/editor/cn/FindPivotIndex.java
@@ -0,0 +1,71 @@
+//给定一个整数类型的数组 nums，请编写一个能够返回数组 “中心索引” 的方法。 
+//
+// 我们是这样定义数组 中心索引 的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。 
+//
+// 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：
+//nums = [1, 7, 3, 6, 5, 6]
+//输出：3
+//解释：
+//索引 3 (nums[3] = 6) 的左侧数之和 (1 + 7 + 3 = 11)，与右侧数之和 (5 + 6 = 11) 相等。
+//同时, 3 也是第一个符合要求的中心索引。
+// 
+//
+// 示例 2： 
+//
+// 输入：
+//nums = [1, 2, 3]
+//输出：-1
+//解释：
+//数组中不存在满足此条件的中心索引。 
+//
+// 
+//
+// 说明： 
+//
+// 
+// nums 的长度范围为 [0, 10000]。 
+// 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。 
+// 
+// Related Topics 数组 
+// 👍 245 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+/**
+ * 724 寻找数组的中心索引
+ * 2021-01-25 07:20:36
+ * 思路：等式判断
+ */
+public class FindPivotIndex {
+    public static void main(String[] args) {
+        Solution solution = new FindPivotIndex().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int pivotIndex(int[] nums) {
+            int left = 0;
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+            for (int i = 0; i < nums.length; i++) {
+                // 重点
+                if (left == sum - nums[i] - left) {
+                    return i;
+                }
+                left += nums[i];
+            }
+            return -1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
