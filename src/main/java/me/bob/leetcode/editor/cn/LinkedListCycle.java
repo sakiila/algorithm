ae99a469a490d5784378b06d5f77a82930d64f72
sakiila <imbaobo@gmail.com>
2021-01-30 09:46:03 +0800

    141 环形链表

--- src/main/java/me/bob/leetcode/editor/cn/LinkedListCycle.java
+++ src/main/java/me/bob/leetcode/editor/cn/LinkedListCycle.java
@@ -80,12 +80,8 @@ public class LinkedListCycle {
      */
     public class Solution {
         public boolean hasCycle(ListNode head) {
-            if (head == null) {
-                return false;
-            }
-
             ListNode slow = head, fast = head;
-            while (fast.next != null && fast.next.next != null) {
+            while (fast != null && fast.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
                 if (slow == fast) {
