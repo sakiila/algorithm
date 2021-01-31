b5afb35b0bda180adf808cba65d21c319fdf926c
sakiila <imbaobo@gmail.com>
2021-01-31 09:01:58 +0800

    203 移除链表元素

---
+++ src/main/java/me/bob/leetcode/editor/cn/RemoveLinkedListElements.java
@@ -0,0 +1,69 @@
+//删除链表中等于给定值 val 的所有节点。 
+//
+// 示例: 
+//
+// 输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+// 
+// Related Topics 链表 
+// 👍 518 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+/**
+ * 203 移除链表元素
+ * 2021-01-31 08:40:04
+ * 思路：遍历，注意else语句
+ */
+public class RemoveLinkedListElements {
+    public static void main(String[] args) {
+        Solution solution = new RemoveLinkedListElements().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode temp = dummy;
+            while (temp.next != null) {
+                if (temp.next.val == val) {
+                    temp.next = temp.next.next;
+                } else {
+                    temp = temp.next;
+                }
+            }
+            return dummy.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
