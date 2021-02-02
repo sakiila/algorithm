e0ce9c10a9600fd3e3687ce925986eebbd9c64d9
sakiila <imbaobo@gmail.com>
2021-02-02 08:01:18 +0800

    138 复制带随机指针的链表

--- src/main/java/me/bob/leetcode/editor/cn/CopyListWithRandomPointer.java
+++ src/main/java/me/bob/leetcode/editor/cn/CopyListWithRandomPointer.java
@@ -85,48 +85,40 @@ class Node {
 
     class Solution {
         public Node copyRandomList(Node head) {
-
             if (head == null) {
                 return null;
             }
 
-            // Creating a new weaved list of original and copied nodes.
-            Node temp = head;
-            while (temp != null) {
-
-                // Cloned node
-                Node newNode = new Node(temp.val);
-
-                // Inserting the cloned node just next to the original node.
-                // If A->B->C is the original linked list,
-                // Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
-                newNode.next = temp.next;
-                temp.next = newNode;
-                temp = newNode.next;
+            // 在每个结点后面复制一个
+            Node p = head;
+            while (p != null) {
+                Node newNode = new Node(p.val);
+                newNode.next = p.next;
+                p.next = newNode;
+                p = p.next.next;
             }
 
-            temp = head;
-
-            // Now link the random pointers of the new nodes created.
-            // Iterate the newly created list and use the original nodes' random pointers,
-            // to assign references to random pointers for cloned nodes.
-            while (temp != null) {
-                temp.next.random = (temp.random != null) ? temp.random.next : null;
-                temp = temp.next.next;
+            // 复制 random 结点
+            p = head;
+            while (p != null) {
+                if (p.random != null) {
+                    p.next.random = p.random.next;
+                }
+                p = p.next.next;
             }
 
-            // Unweave the linked list to get back the original linked list and the cloned list.
-            // i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
-            Node ptr_old_list = head; // A->B->C
-            Node ptr_new_list = head.next; // A'->B'->C'
-            Node head_old = head.next;
-            while (ptr_old_list != null) {
-                ptr_old_list.next = ptr_old_list.next.next;
-                ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
-                ptr_old_list = ptr_old_list.next;
-                ptr_new_list = ptr_new_list.next;
+            // 断开两条链表
+            p = head;
+            Node dummy = new Node(0);
+            Node temp = dummy;
+            while (p != null) {
+                temp.next = p.next;
+                temp = temp.next;
+                p.next = temp.next;
+                p = p.next;
             }
-            return head_old;
+
+            return dummy.next;
         }
     }
 
