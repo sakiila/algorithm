6dc24f38d6d3ab7b122463359f01a2134ec097d7
sakiila <imbaobo@gmail.com>
2021-02-07 07:46:28 +0800

    145 二叉树的后序遍历

---
+++ src/main/java/me/bob/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -0,0 +1,74 @@
+//给定一个二叉树，返回它的 后序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [3,2,1] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 
+// 👍 515 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 145 二叉树的后序遍历
+ * 2021-02-07 07:08:00
+ * 思路：栈
+ */
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreePostorderTraversal().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            Deque<TreeNode> stack = new ArrayDeque<>();
+
+            while (root != null || !stack.isEmpty()) {
+                if (root != null) {
+                    res.add(0, root.val);
+                    stack.push(root);
+                    root = root.right;
+                } else {
+                    root = stack.pop();
+                    root = root.left;
+                }
+            }
+
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
