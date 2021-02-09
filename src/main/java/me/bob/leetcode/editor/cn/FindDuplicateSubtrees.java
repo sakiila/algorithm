1fc8580d6691852dcbde200afb917819dd6dcde4
sakiila <imbaobo@gmail.com>
2021-02-10 07:21:42 +0800

    652 寻找重复的子树

---
+++ src/main/java/me/bob/leetcode/editor/cn/FindDuplicateSubtrees.java
@@ -0,0 +1,92 @@
+//给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。 
+//
+// 两棵树重复是指它们具有相同的结构以及相同的结点值。 
+//
+// 示例 1： 
+//
+//         1
+//       / \
+//      2   3
+//     /   / \
+//    4   2   4
+//       /
+//      4
+// 
+//
+// 下面是两个重复的子树： 
+//
+//       2
+//     /
+//    4
+// 
+//
+// 和 
+//
+//     4
+// 
+//
+// 因此，你需要以列表的形式返回上述重复子树的根结点。 
+// Related Topics 树 
+// 👍 225 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 652 寻找重复的子树
+ * 2021-02-10 07:08:07
+ * 思路：DFS
+ */
+public class FindDuplicateSubtrees {
+    public static void main(String[] args) {
+        Solution solution = new FindDuplicateSubtrees().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        Map<String, Integer> map;
+        List<TreeNode> ans;
+
+        public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+            map = new HashMap<>();
+            ans = new ArrayList<>();
+            collect(root);
+            return ans;
+        }
+
+        private String collect(TreeNode node) {
+            if (node == null) {
+                return "#";
+            }
+            String key = node.val + "," + collect(node.left) + "," + collect(node.right);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+            if (map.get(key) == 2) {
+                ans.add(node);
+            }
+            return key;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
