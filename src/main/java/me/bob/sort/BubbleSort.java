dbd28d08b30400bb3c33c2099b7384f7a563000b
sakiila <imbaobo@gmail.com>
2021-03-19 12:03:45 +0800

    BubbleSort

--- src/main/java/me/bob/sort/BubbleSort.java
+++ src/main/java/me/bob/sort/BubbleSort.java
@@ -8,12 +8,12 @@ public class BubbleSort {
         int[] arr = {2, 5, 43, 51, 1, 3, 6, 20, 18};
         int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] > arr[j]) {
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                 }
             }
         }
