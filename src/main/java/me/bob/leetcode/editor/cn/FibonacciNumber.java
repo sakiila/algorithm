69dee8470a322dcf49949537b86ac51026232c93
sakiila <imbaobo@gmail.com>
2021-03-31 22:54:12 +0800

    509 斐波那契数

---
+++ src/main/java/me/bob/leetcode/editor/cn/FibonacciNumber.java
@@ -0,0 +1,86 @@
+//斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是： 
+//
+// 
+//F(0) = 0，F(1) = 1
+//F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+// 
+//
+// 给你 n ，请计算 F(n) 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：2
+//输出：1
+//解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：3
+//输出：2
+//解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：4
+//输出：3
+//解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 30 
+// 
+// Related Topics 数组 
+// 👍 261 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * 509 斐波那契数
+ * 2021-03-31 22:45:29
+ * 思路：DP + 记忆化
+ */
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        Solution solution = new FibonacciNumber().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int fib(int n) {
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, 0);
+            return helper(n, dp);
+        }
+
+        private int helper(int n, int[] dp) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n == 1 || n == 2) {
+                return 1;
+            }
+            if (dp[n] != 0) {
+                return dp[n];
+            }
+            dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+            return dp[n];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
