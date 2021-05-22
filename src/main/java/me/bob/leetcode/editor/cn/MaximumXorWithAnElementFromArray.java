68e24a3c643629310e1affb189da053ca9f6e4f3
sakiila <imbaobo@gmail.com>
2021-05-23 07:29:34 +0800

    1707 与数组中元素的最大异或值

---
+++ src/main/java/me/bob/leetcode/editor/cn/MaximumXorWithAnElementFromArray.java
@@ -0,0 +1,119 @@
+//给你一个由非负整数组成的数组 nums 。另有一个查询数组 queries ，其中 queries[i] = [xi, mi] 。 
+//
+// 第 i 个查询的答案是 xi 和任何 nums 数组中不超过 mi 的元素按位异或（XOR）得到的最大值。换句话说，答案是 max(nums[j] XOR
+// xi) ，其中所有 j 均满足 nums[j] <= mi 。如果 nums 中的所有元素都大于 mi，最终答案就是 -1 。 
+//
+// 返回一个整数数组 answer 作为查询的答案，其中 answer.length == queries.length 且 answer[i] 是第 i 个
+//查询的答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [0,1,2,3,4], queries = [[3,1],[1,3],[5,6]]
+//输出：[3,3,7]
+//解释：
+//1) 0 和 1 是仅有的两个不超过 1 的整数。0 XOR 3 = 3 而 1 XOR 3 = 2 。二者中的更大值是 3 。
+//2) 1 XOR 2 = 3.
+//3) 5 XOR 2 = 7.
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [5,2,4,6,6,3], queries = [[12,4],[8,1],[6,3]]
+//输出：[15,-1,5]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length, queries.length <= 105 
+// queries[i].length == 2 
+// 0 <= nums[j], xi, mi <= 109 
+// 
+// Related Topics 位运算 字典树 
+// 👍 37 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 1707 与数组中元素的最大异或值
+ * 2021-05-23 07:26:48
+ * 思路：离线询问 + 字典树
+ */
+public class MaximumXorWithAnElementFromArray {
+    public static void main(String[] args) {
+        Solution solution = new MaximumXorWithAnElementFromArray().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] maximizeXor(int[] nums, int[][] queries) {
+            Arrays.sort(nums);
+            int numQ = queries.length;
+            int[][] newQueries = new int[numQ][3];
+            for (int i = 0; i < numQ; ++i) {
+                newQueries[i][0] = queries[i][0];
+                newQueries[i][1] = queries[i][1];
+                newQueries[i][2] = i;
+            }
+
+            Arrays.sort(newQueries, Comparator.comparingInt(a -> a[1]));
+
+            int[] ans = new int[numQ];
+            Trie trie = new Trie();
+            int idx = 0, n = nums.length;
+            for (int[] query : newQueries) {
+                int x = query[0], m = query[1], qid = query[2];
+                while (idx < n && nums[idx] <= m) {
+                    trie.insert(nums[idx]);
+                    ++idx;
+                }
+                if (idx == 0) { // 字典树为空
+                    ans[qid] = -1;
+                } else {
+                    ans[qid] = trie.getMaxXor(x);
+                }
+            }
+            return ans;
+        }
+    }
+
+    class Trie {
+        static final int L = 30;
+        Trie[] children = new Trie[2];
+
+        public void insert(int val) {
+            Trie node = this;
+            for (int i = L - 1; i >= 0; --i) {
+                int bit = (val >> i) & 1;
+                if (node.children[bit] == null) {
+                    node.children[bit] = new Trie();
+                }
+                node = node.children[bit];
+            }
+        }
+
+        public int getMaxXor(int val) {
+            int ans = 0;
+            Trie node = this;
+            for (int i = L - 1; i >= 0; --i) {
+                int bit = (val >> i) & 1;
+                if (node.children[bit ^ 1] != null) {
+                    ans |= 1 << i;
+                    bit ^= 1;
+                }
+                node = node.children[bit];
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
