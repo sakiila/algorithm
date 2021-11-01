1d3fb4fe36bad0264c373c074bcc49f3621ab52c
sakiila <imbaobo@gmail.com>
2021-11-01 17:00:04 +0800

    update file

--- src/main/java/me/bob/test/Solution.java
+++ src/main/java/me/bob/test/Solution.java
@@ -1,10 +1,6 @@
 package me.bob.test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
 
