f415dea5adbf93356e69b7b3003d8d3aaa785983
sakiila <imbaobo@gmail.com>
2021-11-14 09:32:42 +0800

    677 键值映射

---
+++ src/main/java/me/bob/leetcode/editor/cn/MapSumPairs.java
@@ -0,0 +1,108 @@
+//实现一个 MapSum 类，支持两个方法，insert 和 sum： 
+//
+// 
+// MapSum() 初始化 MapSum 对象 
+// void insert(String key, int val) 插入 key-val 键值对，字符串表示键 key ，整数表示值 val 。如果键 
+//key 已经存在，那么原来的键值对将被替代成新的键值对。 
+// int sum(string prefix) 返回所有以该前缀 prefix 开头的键 key 的值的总和。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//["MapSum", "insert", "sum", "insert", "sum"]
+//[[], ["apple", 3], ["ap"], ["app", 2], ["ap"]]
+//输出：
+//[null, null, 3, null, 5]
+//
+//解释：
+//MapSum mapSum = new MapSum();
+//mapSum.insert("apple", 3);  
+//mapSum.sum("ap");           // return 3 (apple = 3)
+//mapSum.insert("app", 2);    
+//mapSum.sum("ap");           // return 5 (apple + app = 3 + 2 = 5)
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= key.length, prefix.length <= 50 
+// key 和 prefix 仅由小写英文字母组成 
+// 1 <= val <= 1000 
+// 最多调用 50 次 insert 和 sum 
+// 
+// Related Topics 设计 字典树 哈希表 字符串 👍 120 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 677 键值映射
+ * 2021-11-14 09:20:39
+ * 思路：字典树
+ */
+public class MapSumPairs {
+    public static void main(String[] args) {
+//        Solution solution = new MapSumPairs().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class MapSum {
+        
+        class TrieNode {
+            int val = 0;
+            TrieNode[] next = new TrieNode[26];
+        }
+        
+        TrieNode root;
+
+        Map<String, Integer> map;
+
+        public MapSum() {
+            root = new TrieNode();
+            map = new HashMap<>();
+        }
+
+        public void insert(String key, int val) {
+            int delta = val - map.getOrDefault(key, 0);
+            map.put(key, val);
+
+            TrieNode node = root;
+            for (char c : key.toCharArray()) {
+                if (node.next[c - 'a'] == null) {
+                    node.next[c - 'a'] = new TrieNode();
+                }
+                node = node.next[c - 'a'];
+                node.val += delta;
+            }
+        }
+
+        public int sum(String prefix) {
+            TrieNode node = root;
+            for (char c : prefix.toCharArray()) {
+                if (node.next[c - 'a'] == null) {
+                    return 0;
+                }
+                node = node.next[c - 'a'];
+            }
+            return node.val;
+        }
+    }
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
