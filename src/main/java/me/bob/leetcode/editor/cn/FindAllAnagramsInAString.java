d0c4b387ebda220eef437b33ffc15d2423dbd64a
sakiila <imbaobo@gmail.com>
2021-11-28 09:28:53 +0800

    438 找到字符串中所有字母异位词

---
+++ src/main/java/me/bob/leetcode/editor/cn/FindAllAnagramsInAString.java
@@ -0,0 +1,91 @@
+//给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。 
+//
+// 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "cbaebabacd", p = "abc"
+//输出: [0,6]
+//解释:
+//起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+//起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "abab", p = "ab"
+//输出: [0,1,2]
+//解释:
+//起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+//起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+//起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length, p.length <= 3 * 10⁴ 
+// s 和 p 仅包含小写字母 
+// 
+// Related Topics 哈希表 字符串 滑动窗口 👍 662 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 438 找到字符串中所有字母异位词
+ * 2021-11-28 08:23:35
+ * 思路：滑动窗口
+ */
+public class FindAllAnagramsInAString {
+    public static void main(String[] args) {
+        Solution solution = new FindAllAnagramsInAString().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Integer> findAnagrams(String s, String p) {
+            List<Integer> result = new ArrayList<>();
+
+            final int sLen = s.length();
+            final int pLen = p.length();
+
+            if (sLen < pLen) {
+                return result;
+            }
+
+            int[] sCnt = new int[26];
+            int[] pCnt = new int[26];
+
+            for (int i = 0; i < pLen; i++) {
+                sCnt[s.charAt(i) - 'a']++;
+                pCnt[p.charAt(i) - 'a']++;
+            }
+
+            for (int i = 0; i < sLen - pLen + 1; i++) {
+                if (Arrays.equals(sCnt, pCnt)) {
+                    result.add(i);
+                }
+                if (i + pLen >= sLen) {
+                    continue;
+                }
+                sCnt[s.charAt(i) - 'a']--;
+                sCnt[s.charAt(i + pLen) - 'a']++;
+            }
+
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
