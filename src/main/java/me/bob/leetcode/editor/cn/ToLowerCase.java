cf90b31bf3fe40e5ea014c965a701200e507bbf3
sakiila <imbaobo@gmail.com>
2021-12-12 11:28:21 +0800

    709 转换成小写字母

---
+++ src/main/java/me/bob/leetcode/editor/cn/ToLowerCase.java
@@ -0,0 +1,64 @@
+//给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "Hello"
+//输出："hello"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "here"
+//输出："here"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "LOVELY"
+//输出："lovely"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 100 
+// s 由 ASCII 字符集中的可打印字符组成 
+// 
+// Related Topics 字符串 👍 180 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+/**
+ * 709 转换成小写字母
+ * 2021-12-12 11:14:01
+ * 思路：异或 32
+ */
+public class ToLowerCase {
+    public static void main(String[] args) {
+        Solution solution = new ToLowerCase().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String toLowerCase(String s) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                if (c >= 65 && c <= 90) {
+                    c |= 32;
+                }
+                sb.append(c);
+            }
+            return sb.toString();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
