a9fe2261ad2d1c58e1b2ed38d042f1c794582909
sakiila <imbaobo@gmail.com>
2022-04-24 18:21:34 +0800

    680 验证回文字符串 Ⅱ

---
+++ src/main/java/me/bob/leetcode/editor/cn/ValidPalindromeIi.java
@@ -0,0 +1,78 @@
+//给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "aba"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "abca"
+//输出: true
+//解释: 你可以删除c字符。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "abc"
+//输出: false 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s 由小写英文字母组成 
+// 
+// Related Topics 贪心 双指针 字符串 👍 490 👎 0
+
+
+package me.bob.leetcode.editor.cn;
+
+/**
+ * 680 验证回文字符串 Ⅱ
+ * 2022-04-24 18:19:05
+ * 思路：双指针
+ */
+public class ValidPalindromeIi {
+    public static void main(String[] args) {
+        Solution solution = new ValidPalindromeIi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean validPalindrome(String s) {
+            int left = 0, right = s.length() - 1;
+            while (left <= right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    // 取左边部分或右边部分
+                    return validPalindrome(s, left, right - 1) || validPalindrome(s, left + 1, right);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        private boolean validPalindrome(String s, int left, int right) {
+
+            while (left <= right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
