59e43bd5212a848c728d6097e10079d0faed0daa
sakiila <me@baobo.me>
2022-07-14 08:48:18 +0800

    fix

--- src/main/java/me/bob/test/A.java
+++
@@ -1,7 +0,0 @@
-package me.bob.test;
-
-public interface A {
-    default int test() {
-        return 1;
-    }
-}
--- src/main/java/me/bob/test/B.java
+++
@@ -1,7 +0,0 @@
-package me.bob.test;
-
-public interface B {
-    default int test() {
-        return 2;
-    }
-}
